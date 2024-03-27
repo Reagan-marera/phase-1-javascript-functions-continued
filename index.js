7b905b0a69ed797fd8b1c2f3dd082c914fc6e0b9
reagan <marierareagan@gmail.com>
2024-03-27 15:30:49 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+// Function to implement saturdayFun with a default activity
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+console.log(saturdayFun('picnicking'));
+
+function mondayWork(task = 'go to the office') {
+   return  `This Monday, I will ${task}.`;
+}
+
+
+mondayWork('complete the project');
+
+
+function wrapAdjective(flair = '*') {
+    return function(adjective = 'special') {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
